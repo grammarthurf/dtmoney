899c2ad71863ebbff423b93f2dc4e4bd01dc305c
Arthur Gramm <arthurgramm@outlook.com.br>
2021-05-21 20:53:59 -0300

    isolated component for modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 
-import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
-
-Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -25,12 +23,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
