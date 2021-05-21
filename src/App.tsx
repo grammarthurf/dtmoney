f5e1217d55bd73c43657aa23aa12e23ee439ca55
Arthur Gramm <arthurgramm@outlook.com.br>
2021-05-20 21:14:26 -0300

    class 07 - create header of application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from './components/Header';
+
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Dtmoney</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 };
