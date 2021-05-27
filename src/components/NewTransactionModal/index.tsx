553e4d8be354730db95a2e6e954d63e03ba7ad7d
Arthur Gramm <arthurgramm@outlook.com.br>
2021-05-26 23:09:51 -0300

    using miragejs methods get, post and local DB

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,12 @@
 import { FormEvent, useState } from 'react';
+
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+
+import { api } from '../../services/api';
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
@@ -24,12 +28,14 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type,
-    })
+    };
+
+    api.post('/transactions', data);
   }
 
   return (
